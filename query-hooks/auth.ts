888a93cddd1cb90dc5eb70708146936a895c8e13
ZkunCry <aksentev.03@mail.ru>
2024-11-13 00:28:03 +0500

    added signup hook

--- query-hooks/auth.ts
+++ query-hooks/auth.ts
@@ -1,21 +1,34 @@
 import type { IUser } from "@/types/user";
 import { AuthService } from "@/services/auth/authService";
 import { useMutation, UseMutationResult } from "@tanstack/react-query";
-import type { SignInType } from "@/schemas/authschema";
+import type { SignInType, SignUpType } from "@/schemas/authschema";
 import toast from "react-hot-toast";
 import type { AxiosError } from "axios";
 
 // Тип для результата мутации
 type IUseSignIn = UseMutationResult<IUser, AxiosError, SignInType, unknown>;
+type IUseSignUp = UseMutationResult<IUser, AxiosError, SignUpType, unknown>;
 
 export function useSignIn(): IUseSignIn {
   return useMutation<IUser, AxiosError, SignInType>({
     mutationFn: AuthService.signIn,
-    onError: (error: AxiosError) => {
+    onError: (error) => {
       toast.error(`Ошибка: ${error.response?.data}`);
     },
     onSuccess: (data) => {
       toast.success("Вы успешно вошли в систему.");
+    },
+  });
+}
+
+export function useSignUp(): IUseSignUp {
+  return useMutation<IUser, AxiosError, SignUpType>({
+    mutationFn: AuthService.signUp,
+    onError: (error) => {
+      toast.error(`Ошибка: ${error.response?.data}`);
+    },
+    onSuccess: (data) => {
+      toast.success("Вы успешно прошли авторизацию.");
     },
   });
 }
